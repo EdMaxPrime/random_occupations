5b1514609ce56fcc68af0231610d054337ee2385
Max Z <max.zlotskiy@gmail.com>
2017-09-14 21:39:35 -0400

    parseCSV now correctly works with quoted values containing commas

--- workforce.py
+++ workforce.py
@@ -6,8 +6,19 @@ def parseCSV(csv, includeFirstLine):
     for row in rows:
         row = row.strip()
         if(len(row) > 0):
-            valuesByRow.append(row.split(","))
-    print valuesByRow
+            columns = [""]
+            quote = False
+            for c in row:
+                if(c == '"' and quote == False):
+                    quote = True
+                elif(c == '"' and quote == True):
+                    quote = False
+                elif(c == ',' and quote == False):
+                    columns.append("")
+                else:
+                    columns[-1] += c
+            valuesByRow.append(columns)
+    return valuesByRow
 
 file = open("occupations.csv", 'r')
 parseCSV(file.read(), True)
