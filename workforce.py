69fab4943e012d2006995112db24b29e69f9629a
Max Z <max.zlotskiy@gmail.com>
2017-09-14 21:34:02 -0400

    introduced parseCSV, which turns csv file into 2D list

--- workforce.py
+++ workforce.py
@@ -1,3 +1,14 @@
+def parseCSV(csv, includeFirstLine):
+    rows = csv.split("\n")
+    if(not includeFirstLine):
+        rows = rows[1:]
+    valuesByRow = []
+    for row in rows:
+        row = row.strip()
+        if(len(row) > 0):
+            valuesByRow.append(row.split(","))
+    print valuesByRow
+
 file = open("occupations.csv", 'r')
-print file.read()
+parseCSV(file.read(), True)
 file.close()
